ef277238406d7b66ab37966bcfa8a069009b47d8
Rafna Bash <rafnabash@gmail.com>
2018-05-15 19:58:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,12 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var newChocolateBars = [1]
 
-function addElementToBeginningOfArray(){
-  newChocolateBars.unshift("foo")
-  return newChocolateBars
-}
-
-function destructivelyAddElementToBeginningOfArray(){
-  newChocolateBars = ["foo", ...newChocolateBars]
-  return newChocolateBars
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]  
 }
